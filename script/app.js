a56aa5f75e78f19aec802865cfb3499109441ac6
David Rivadeneyra <david.rivadeneyra.c@gmail.com>
2019-09-16 13:55:16 -0500

    jeje

--- script/app.js
+++ script/app.js
@@ -16,7 +16,7 @@ window.addEventListener('load', () => {
             long = position.coords.longitude
             // API https://darksky.net/dev/account
             const proxy = 'https://cors-anywhere.herokuapp.com/'
-            const api = ${proxy}`https://api.darksky.net/forecast/9a30e09ae522e017b753f337d584aab5/${lat},${long}`
+            const api = `${proxy}https://api.darksky.net/forecast/9a30e09ae522e017b753f337d584aab5/${lat},${long}`
 
             fetch(api).then(responseFromApi => {
                 return responseFromApi.json()
